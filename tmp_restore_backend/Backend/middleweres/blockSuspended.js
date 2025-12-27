bde2b3f366af2c65ade9f9eefc7f285e0ed0eb51
Mahmud <tech.trust@gmail.com>
2025-12-27 19:54:50 +0100

    chore: cleanup temp restore artifacts

--- tmp_restore_backend/Backend/middleweres/blockSuspended.js
+++
@@ -1,8 +0,0 @@
-exports.blockSuspended = (req, res, next) => {
-  if (req.user.isSuspended) {
-    return res.status(403).json({
-      message: "Account suspended due to fraudulent activity"
-    })
-  }
-  next()
-}
