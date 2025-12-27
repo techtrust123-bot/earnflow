aa76cb7f22d5c444da0e9ea0dd77770d8641aa38
Mahmud <tech.trust@gmail.com>
2025-12-27 18:33:33 +0100

    chore: remove duplicate Backend folder (casing conflict)

--- Backend/routes/withdraw.js
+++
@@ -1,9 +0,0 @@
-const { withdraw } = require("../controllers/withdrawController");
-const { authMiddlewere } = require("../middleweres/authmiddlewere");
-
-const router = require("express").Router();
-
-
-router.post("/request",authMiddlewere,withdraw)
-
-module.exports = router;
