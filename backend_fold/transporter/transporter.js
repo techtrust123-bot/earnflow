1a210d4ee7f06e5fe243426cb556a51dbc45f82d
Mahmud <tech.trust@gmail.com>
2025-12-31 17:23:37 +0100

    trasporter fix

--- backend_fold/transporter/transporter.js
+++ backend_fold/transporter/transporter.js
@@ -1,13 +1,10 @@
 const nodemailer = require("nodemailer")
 
 const transporter = nodemailer.createTransport({
-    host:"smtp-relay.brevo.com",
-    port:587,
-    secure:false,
-
+    service: "gmail",
     auth:{
-        user:process.env.SMTP_USER,
-        pass:process.env.SMTP_PASS
+        user:process.env.GMAIL_USER,
+        pass:process.env.GMAIL_PASS
     }
 })
 
