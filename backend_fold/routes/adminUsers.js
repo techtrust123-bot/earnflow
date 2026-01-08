019055f84295e562d45f65133f6a7f626b770b9d
Mahmud <tech.trust@gmail.com>
2026-01-08 21:53:36 +0100

    enable role update

--- backend_fold/routes/adminUsers.js
+++ backend_fold/routes/adminUsers.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { authMiddlewere } = require('../middleweres/authmiddlewere')
 const authorizeRoles = require('../middleweres/roleMiddlewere')
-const { getUsers, getStats } = require('../controllers/adminUsers')
+const { getUsers, getStats, deleteUser, updateUserRole } = require('../controllers/adminUsers')
 
 router.get('/users', authMiddlewere, authorizeRoles('admin'), getUsers)
 router.get('/stats', authMiddlewere, authorizeRoles('admin'), getStats)
-router.delete('/users/:id', authMiddlewere, authorizeRoles('admin'), require('../controllers/adminUsers').deleteUser)
+router.delete('/users/:id', authMiddlewere, authorizeRoles('admin'), deleteUser)
+router.patch('/users/:id/role', authMiddlewere, authorizeRoles('admin'), updateUserRole)
 
 module.exports = router
